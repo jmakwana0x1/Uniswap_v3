fcfc93f25464322f747faad6f0b9f3b47b099dab
jmakwana0x1 <jaymakwanaworks@gmail.com>
2026-02-09 04:04:22 +0000

    feat: add DeployDevelopment script and UniswapV3Manager contract with mint and swap functionality

---
+++ script/DeployDevelopment.s.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+
+import {UniswapV3Pool} from"../src/UniswapV3Pool.sol";
+import  {UniswapV3Manager} from "../src/UniswapV3Manager.sol";
+import {ERC20Mintable} from "test/ERC20Mintable.sol";
+contract DeployDevelopmentScript  is Script{
+    function run()public{
+        uint256 wethBalance = 1 ether;
+        uint256 usdcBalance = 5042 ether;
+        int24 currentTick = 85176;
+        uint160 currentSqrtP = 5602277097478614198912276234240;
+         vm.startBroadcast();
+        ERC20Mintable token0 = new ERC20Mintable("Wrapped Ether", "WETH", 18);
+        ERC20Mintable token1 = new ERC20Mintable("USD Coin", "USDC", 18);
+
+        UniswapV3Pool pool = new UniswapV3Pool(
+            address(token0),
+            address(token1),
+            currentSqrtP,
+            currentTick
+        );
+
+        UniswapV3Manager manager = new UniswapV3Manager();
+
+        token0.mint(msg.sender, wethBalance);
+        token1.mint(msg.sender, usdcBalance);
+
+        vm.stopBroadcast();
+
+        console.log("WETH address", address(token0));
+        console.log("USDC address", address(token1));
+        console.log("Pool address", address(pool));
+        console.log("Manager address", address(manager));
+    }
+    
+}
